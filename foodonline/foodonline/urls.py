561329522141cf10cead217feaf82609ae9be87b
vinodkarnat <vinod90python@gmail.com>
2023-09-13 12:16:14 +0530

    first commit

--- foodonline/foodonline/urls.py
+++ foodonline/foodonline/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for foodonline_main project.
+URL configuration for foodonline project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -20,5 +20,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",views.home,name="home"),
+    path("",views.home,name="home")
 ]
