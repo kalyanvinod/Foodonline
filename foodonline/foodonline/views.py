5d71790657d0a36c8aa76d26839a34f571bdcef7
vinodkarnat <vinod90python@gmail.com>
2023-09-13 13:17:51 +0530

    add new functionality

--- foodonline/foodonline/views.py
+++ foodonline/foodonline/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("<h1>This is MyFirst Application")
+
+
+
+def about(request):
+    return HttpResponse("<h2>This is About Page<h2>")
