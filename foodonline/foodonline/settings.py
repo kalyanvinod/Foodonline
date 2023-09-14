f9c8d6559c7714977334a3d0c1140db4aa43e8de
vinodkarnat <vinod90python@gmail.com>
2023-09-14 12:12:40 +0530

    Index page added

--- foodonline/foodonline/settings.py
+++ foodonline/foodonline/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'foodonline.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR,'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'static/'
+STATICFILES_DIRS = [
+    'foodonline/static/'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- foodonline/foodonline/views.py
+++ foodonline/foodonline/views.py
@@ -1,8 +1,8 @@
-
+from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>This is MyFirst Application")
+    return render(request, "index.html")
 
 
 
